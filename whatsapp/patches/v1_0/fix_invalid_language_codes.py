a1b34563c40a6cdb355b1fd908097d3df99a5417
Pratham <prathamsharma173@gmail.com>
2026-08-31 16:31:36 +0530

    fix: remove db.commit from patch file

--- whatsapp/patches/v1_0/fix_invalid_language_codes.py
+++ whatsapp/patches/v1_0/fix_invalid_language_codes.py
@@ -7,5 +7,3 @@ def execute():
 	"""`en_UK` was offered in the old Select options but Meta never accepted it."""
 	for old, new in RETIRED_LANGUAGE_CODES.items():
 		frappe.db.set_value("WhatsApp Template", {"language": old}, "language", new, update_modified=False)
-
-	frappe.db.commit()
